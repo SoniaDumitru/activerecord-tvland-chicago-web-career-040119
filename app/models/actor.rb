ffa09d33e94d4abda4927e39ce4682b70f8db70e
github username <github email address>
2019-04-13 16:12:30 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,16 @@
 class Actor < ActiveRecord::Base
-  
+#can build its associated characters
+  has_many :characters
+#can build its associated shows through its characters
+  has_many :shows, through: :characters
+#list its full name
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+#can list all of its shows and characters
+  def list_roles
+    self.characters.map do |character|
+       "#{character.name} - #{character.show.name}"
+  end.join(", ")
+end
 end
